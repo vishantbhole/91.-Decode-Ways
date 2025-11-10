00d2ca392675483a72c1e3057b090218a25c4cd7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-09 21:15:47 -0600

    Implement dynamic programming for decoding ways

--- numDecodings.py
+++ numDecodings.py
@@ -2,4 +2,12 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        
+        dp ={len(s) : 1}
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
 
